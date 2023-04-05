72b82f7617871e04e0cdd5e0922ad1ed5d0bb7f0
Alex Iliescu <iliescualexandrurobert@gmail.com>
2023-04-05 18:58:02 +0300

    header and sidebar

--- Content/Scripts/beat.template.js
+++ Content/Scripts/beat.template.js
@@ -3,12 +3,78 @@ BEAT Template
 */
 
 /* globals Chart:false, feather:false */
+// collapse sidebar start
 
 var collapseBtn = document.querySelector(".btn-collapse");
 var sidebar = document.getElementById("sidebarMenu");
+var searchBar = document.querySelector(".beat-search");
 collapseBtn.addEventListener('click', function handleClick(event) {
   sidebar.classList.toggle("beat-micro-sidebar");
+  searchBar.classList.toggle("shift-search");
+  init_tooltip();
 });
+// collapse sidebar end
+
+// initialize tooltips start
+
+function init_tooltip(){
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+  });
+  if(sidebar.classList.contains('beat-micro-sidebar')){
+    // console.log("Micro sidebar");
+    
+  }else{
+    // console.log("Large Sidebar");
+    tooltipList.forEach((tooltip) => {tooltip.dispose()});
+  }
+  
+}
+// initialize tooltips end
+
+// fullscreen start
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+var btn_maximize = document.querySelector(".fa-expand");
+var btn_minimize = document.querySelector(".fa-minimize");
+
+btn_maximize.addEventListener('click', function handleClick(event) {
+  // console.log("fullscreen");
+  btn_minimize.classList.remove("d-none");
+  btn_maximize.classList.add("d-none");
+  openFullscreen();
+});
+
+btn_minimize.addEventListener('click', function handleClick(event) {
+  // console.log("minimize");
+  btn_maximize.classList.remove("d-none");
+  btn_minimize.classList.add("d-none");
+  closeFullscreen();
+});
+// fullscreen end
 
 (function () {
   'use strict'
